c542d1445c4fe4ebff53c386d78a518e36401fad
Andrej Frank - ITD <stz-softwaretech.frank@extaccount.com>
2019-09-23 09:31:32 +0200

    SimpleList del element Todo:  - sort  - find  - template?

--- Data.h
+++ Data.h
@@ -5,28 +5,30 @@ class Data
 {
 private:
 	int wert;
-    Data *next;
+	Data *next;
 
 public:
 	Data(int n) {
-        wert = n;
-        next=nullptr;
+		wert = n;
+		next = nullptr;
 	}
-    Data* getNext() {
-        return next;
-    }
-    void setNext(Data *d) {
-        next = d;
-    }// nothing found or list is void
-    void Set(int n) {
-        wert = n;
-    }
-    int Get(){
-        return wert;
-    }
+	Data* getNext() {
+		return next;
+	}
+	void setNext(Data *d) {
+		next = d;
+	}// nothing found or list is void
+	void Set(int n) {
+		wert = n;
+	}
+	int Get() {
+		return wert;
+	}
 };
 
 ////////////////////////////////////////
+// SimpleList
+
 #pragma once
 #include <iostream>
 #include "Data.h"
@@ -34,139 +36,183 @@ using namespace std;
 class SimpleList
 {
 private:
-    Data *header;
+	Data *header;
 
 public:
-    SimpleList() {
-        header = nullptr;// nothing found or list is void
-    }
-    ~SimpleList() {
-        // TODO: alle delete
-    }
+	SimpleList() 
+	{
+		header = nullptr;// nothing found or list is void
+	}
+	~SimpleList() 
+	{
+		// TODO: alle delete
+	}
 
-    Data* getLast() {
-        if (header == nullptr )
-            return nullptr;
+	Data* getLast() 
+	{
+		if (header == nullptr)
+			return nullptr;
 
-        Data *d = header;
-        while (d->getNext() != nullptr)
-            d=d->getNext();
-        return d;
-    }
+		Data *d = header;
+		while (d->getNext() != nullptr)
+			d = d->getNext();
+		return d;
+	}
 
-    Data* find(int n) {
-        Data *tmp = header;
-        while(tmp!=nullptr) {
-            if ( tmp->Get() == n )
-                return tmp;
-            tmp = tmp->getNext();
-        }// nothing found or list is void
-        return tmp;
-    }
+	Data* find(int n) 
+	{
+		Data *tmp = header;
+		while (tmp != nullptr) 
+		{
+			if (tmp->Get() == n)
+				return tmp;
+			tmp = tmp->getNext();
+		}// nothing found or list is void
+		return tmp;
+	}
 
-    Data* findPrevious(Data *n) {
-        Data *tmp = header;
-        if (tmp == nullptr)  // list is void
-            return nullptr;
-        if (tmp->getNext() == nullptr ) // list has only one element
-            return nullptr;
-        while(tmp->getNext() == n)
-            tmp = tmp->getNext();
-        return tmp;
-    }
+	Data* findPrevious(Data *n) 
+	{
+		Data *tmp = header;
+		if (tmp == nullptr)  // list is void
+			return nullptr;
+		if (tmp->getNext() == nullptr) // list has only one element
+			return nullptr;
+		while (tmp->getNext() == n)
+			tmp = tmp->getNext();
+		return tmp;
+	}
 
-    void add(int n) {
-        Data *d = new Data(n);
-        Data *last = getLast();
-        if ( last == nullptr)
-            header = d;
-        else
-            last->setNext(d);
-    }
+	void add(int n) 
+	{
+		Data *d = new Data(n);
+		Data *last = getLast();
+		if (last == nullptr)
+			header = d;
+		else
+			last->setNext(d);
+	}
 
-    bool del(int n) {
-        Data *tmp = find(n);
-        if (tmp == nullptr)  // nothing found or list is void
-            return false;
-        
-        if ( tmp == header ) { // element is header or one elemnt
-            header = header->getNext();
-            delete tmp;
-            return true;
-        }
+	bool del(int n) 
+	{
+		Data *tmp = header;
+		if (tmp == nullptr)  // nothing found or list is void
+			return false;
 
-        Data *prev = findPrevious(tmp);  // more than 1 element
+		if (tmp->getNext() == nullptr) { // one elemnt
+			if (tmp->Get() == n) 
+			{ // found
+				header = nullptr;
+				delete tmp;
+				return true;
+			}
+			return false;
+		}
 
-        cout << "DEBUG: prev: 0x" << prev << " (" << prev->Get();
-        cout << ") -> tmp 0x" << prev->getNext();
-        cout << " [ 0x" << tmp << " ("<< tmp->Get()<<")]   ";
-        prev->setNext(tmp->getNext());
-        delete tmp;
-        return true;
-    }
+		// first element
+		if (tmp->Get() == n) 
+		{
+			header = tmp->getNext();
+			delete tmp;
+			return true;
+		}
 
-    void print() {
-        if ( header == nullptr ) {
-            cout << "List is void\n";
-        } else {
-            Data *tmp=header;
-            while(tmp!=nullptr) {
-                cout << tmp->Get() << " :  0x" << tmp << " -> 0x"<< tmp->getNext() <<endl;
-                tmp = tmp->getNext();
-            }
-        }
-        cout<<"----------------------------------------------\n";
-    }
+		// more than 1 element
+		while (tmp->getNext() != nullptr) 
+		{
+			Data *tmp2;
+			tmp2 = tmp->getNext();
+			if (tmp2->Get() == n) // found
+			{
+				tmp->setNext(tmp2->getNext());
+				delete tmp2;
+				return true;
+			}
+			tmp = tmp->getNext();
+		}
+		
+		return false;
+	}
+
+	void debug(Data *prev)
+	{
+		Data *tmp = prev->getNext();
+		cout << "DEBUG: prev: 0x" << prev << " (" << prev->Get();
+		cout << ") -> tmp 0x" << prev->getNext();
+		cout << " [ 0x" << tmp << " (" << tmp->Get() << ")]   ";
+		prev->setNext(tmp->getNext());
+		delete tmp;
+	}
+
+	void print() {
+		if (header == nullptr) {
+			cout << "List is void\n";
+		}
+		else {
+			Data *tmp = header;
+			while (tmp != nullptr) {
+				cout << tmp->Get() << " :  0x" << tmp << " -> 0x" << tmp->getNext() << endl;
+				tmp = tmp->getNext();
+			}
+		}
+		cout << "----------------------------------------------\n";
+	}
 };
 
 
 
 ///////////////////////////////////
 // main
+
+#include "stdafx.h"
 #include <stdio.h>
-#include "Data.h"
+#include <iostream>
+#include "SimpleList.h"
 
 using namespace std;
 
 int main()
 {
-    SimpleList sl;
+	SimpleList sl;
 
-    sl.print();
-    cout << "del(1): "<<sl.del(1)<<endl;
-    sl.add(1);
-    cout << "del(2): "<<sl.del(2)<<endl;
-    sl.print();
+	sl.print();
+	cout << "del(1): " << sl.del(1) << endl;
+	sl.add(1);
+	cout << "del(2): " << sl.del(2) << endl;
+	sl.print();
 
-    cout << "del(1): "<<sl.del(1)<<endl;
-    sl.print();
-    
-    sl.add(1);
-    sl.add(22); 
-    sl.add(33);
-    sl.print();
-    
-    cout << "del(1): "<<sl.del(1)<<endl;
-    sl.print();
+	cout << "del(1): " << sl.del(1) << endl;
+	sl.print();
 
-    sl.add(55); sl.add(40);
-    sl.print();
-    cout << "del(33): "<<sl.del(33)<<endl;
-    sl.print();
-    cout << "del(1): "<<sl.del(1)<<endl;
-    sl.print();
-    cout << "del(55): "<<sl.del(55)<<endl;
-    sl.print();
-    return 0;
+	sl.add(1);
+	sl.add(22);
+	sl.add(33);
+	sl.print();
 
+	cout << "del(1): " << sl.del(1) << endl;
+	sl.print();
 
-    for(int i(0); i<10; i++)
-        sl.add(i);
-    sl.del(8);
-    sl.add(55);
-    sl.print();
+	sl.add(55); sl.add(40);
+	sl.print();
+	sl.print();
+	cout << "del(33): " << sl.del(33) << endl;
+	sl.print();
+	cout << "del(1): " << sl.del(1) << endl;
+	sl.print();
+	cout << "del(55): " << sl.del(55) << endl;
+	sl.print();
+	cout << "del(40): " << sl.del(40) << endl;
+	sl.print();
+	cout << "del(22): " << sl.del(22) << endl;
+	sl.print();
 
-    return 0;
+	for (int i(0); i<10; i++)
+		sl.add(i);
+	sl.del(8);
+	sl.add(55);
+	sl.print();
+
+	return 0;
 }
 
 
